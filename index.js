bbbdb2d31fd58e87b1e1e18ffa994ce2a2a4cd16
y0nei <xyz.im@proton.me>
2023-03-07 16:30:12 +0100

    working squishable cat :3

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 let catsquish = document.getElementById("catsquish")
-let catsquishBgColor = catsquish.style.backgroundColor;
+let catsquishimg = document.getElementById("catimg")
+let stillframe = "./src/catsquish.png"
+let animation = "./src/catsquish-single-cropped-gif.gif"
+let running = false
 
 catsquish.addEventListener('click', () => {
-    catsquish.style.backgroundColor = 'blue';
-    console.log("🐱");
-
+    if (running) {
+        return;
+    }
+    catsquishimg.src = animation
+    running = true
+    catsquish.style.backgroundColor = "red"
     setTimeout(() => {
-        catsquish.style.backgroundColor = catsquishBgColor;
-    }, 150);
+        if (running) {
+            catsquishimg.src = stillframe
+            running = false
+            catsquish.style.backgroundColor = "blue"
+        }
+    }, 1500);
 });
