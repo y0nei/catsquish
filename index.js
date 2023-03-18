b31fabf8173fab0d242b74f298cefd459a217fbb
y0nei <xyz.im@proton.me>
2023-03-18 21:54:03 +0100

    improved animations, syntax, styling & fixes

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 let catsquish_container = document.getElementById("catsquish-container");
 let catsquish = document.getElementById("catsquish");
-let counterEl = document.querySelectorAll("#squishcount > p");
+let squishcounter = document.querySelectorAll("#squishcount > p");
 let stillframe = "./src/still_frame.png";
 var counter = 0;
 
+// Animation helpers
+
 function play(el) {
-  el.style.animationPlayState = 'running';
+    el.style.animationPlayState = 'running';
 }
 function pause(el) {
-  el.style.animationPlayState = 'paused';
+    el.style.animationPlayState = 'paused';
 }
 function reset(el) {
-  el.style.animation = 'none';
-  el.offsetHeight; /* trigger reflow to apply the change immediately */
-  el.style.animation = null;
+    el.style.animation = 'none';
+    el.offsetHeight; /* trigger reflow to apply the change immediately */
+    el.style.animation = null;
+}
+function stop(el) {
+    reset(el);
+    pause(el);
 }
 
 // Audio controller (with overlapping)
@@ -23,46 +29,57 @@ let boingSound = "boing.mp3"
 let playingAudio = [];
 
 function playAudio() {
-  if (playingAudio.length < maxAudioPlaying) {
-    let audio = new Audio(boingSound);
-    audio.volume = 1;
-    playingAudio.push(audio);
-    audio.play();
-    audio.addEventListener('ended', function() {
-        var index = playingAudio.indexOf(audio);
-        if (index > -1) {
-            playingAudio.splice(index, 1);
-        }
-    });
-  }
+    if (playingAudio.length < maxAudioPlaying) {
+        let audio = new Audio(boingSound);
+        audio.volume = 1;
+        playingAudio.push(audio);
+        audio.play();
+        audio.addEventListener('ended', function() {
+            var index = playingAudio.indexOf(audio);
+            if (index > -1) {
+                playingAudio.splice(index, 1);
+            }
+        });
+    }
 }
 
 // Bounce animation controller (with overalpping)
+
+/*
+* TODO: Still needs fixes, the animations do work properly
+* but after a few cycles, it gets janky and the animations reset
+*/
 
 let animElements = document.querySelectorAll(".anim-container");
 
 function playBounce() {
     let notRunning = Array.from(animElements).filter(el => el.style.animationPlayState !== "running");
-
     if (notRunning.length > 0) {
         let nextAnimElement = notRunning[0];
         reset(nextAnimElement);
         play(nextAnimElement);
         nextAnimElement.addEventListener("animationend", () => {
-            pause(nextAnimElement);
+            stop(nextAnimElement);
         }, {once: true});
     }
 }
 
+// Disable any animations that would play on window load
 window.addEventListener("load", () => {
-    reset(catsquish);
-    pause(catsquish);
+    stop(catsquish);
+    for (let i = 0; i < animElements.length; i++) {
+        stop(animElements[i]);
+    }
 });
 
-catsquish_container.addEventListener('click', () => {
+// mouse event on container
+catsquish_container.addEventListener('mousedown', () => {
     reset(catsquish);
+    // Delay for better reapeatability
+    catsquish.style.animationDelay = "-100ms";
+
     playAudio();
     playBounce();
     counter++;
-    counterEl[0].innerText = counter;
+    squishcounter[0].innerText = counter;
 });
